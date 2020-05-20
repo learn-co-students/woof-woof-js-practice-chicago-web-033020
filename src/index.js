a916e5b425186a10478df76fe8c421fffa9700c1
Shau Yin Luk <ivan.luk@icloud.com>
2020-05-20 09:52:49 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -5,72 +5,71 @@ let filterOn = "off"
 
 let allPups = []
 function main(){
-  fetch('http://localhost:3000/pups')
-  .then(resp => resp.json())
-  .then(pups => {
-    allPups = pups
-    renderPups(pups)});
+  getPups()
 
   dogBar.addEventListener("click", handleClick)
   dogInfo.addEventListener("click", handleToggle)
   dogFilter.addEventListener("click", filterClick)
 }
 
+// get all the pups and render them
+function getPups(){
+  fetch('http://localhost:3000/pups')
+  .then(resp => resp.json())
+  .then(pups => {
+    allPups = pups
+    renderPups(pups)})
+}
+
+// this handle the click of the on/off toggle for good/bad dog
 function filterClick(event){
   let status = event.target.innerText.split(":")[1]
   const label = event.target.innerText.split(":")[0]
-  // debugger
  
+  // set the master switch condition to on or off
   if (status === " OFF"){
     event.target.innerText = label + ": ON" 
-    //  filpups = allPups.filter(pup => pup.isGoodDog === true )
     filterOn = "on"
   } else {
     event.target.innerText = label + ": OFF" 
-    // filpups = allPups
     filterOn = "off"
   }
+  // render the dog labels
   dogBar.innerHTML = ""
   renderPups(allPups)
-  // filpups.forEach(pup => renderOnePup(pup))
 }
 
 function renderPups(pups){
-  // debugger
+  // set up the array to be rendered from based on whether the filter is on or off
   let allPups = []
   if (filterOn === "on"){
     allPups = pups.filter(pup => pup.isGoodDog === true )
-    // filterOn = "off"
   } else {
     allPups = pups
-    // filterOn = "on"
   }
   dogBar.innerHTML = ""
   allPups.forEach(pup => renderOnePup(pup))
   
 }
 
-// function renderfilPups(filpups){
-//   filpups.forEach(pup => renderOnePup(pup))
-  
-// }
-
+// this render each dog label
 function renderOnePup(pup){
-  // debugger
   let sp = `<span data-id=${pup.id}>${pup.name} </span>`
   dogBar.innerHTML += sp
 }
 
+// this handles the click on each dog label
 function handleClick(event){
-  // debugger
-  // console.log(event)
+  
   let labelD = ""
 
+  // this determines if the dog clicked is a good or bad dog
   if (allPups[parseInt(event.target.dataset.id) - 1].isGoodDog){
     labelD = "Good Dog!"
   } else {
     labelD = "Bad Dog!"
   }
+  // this display the dog info
   let div = `<img src=${allPups[parseInt(event.target.dataset.id) - 1].image}>` +
   `<h2>${allPups[parseInt(event.target.dataset.id) - 1].name}` + `
   </h2> <button data-dog-id=${event.target.dataset.id}> ${labelD} </button>`
@@ -78,15 +77,12 @@ function handleClick(event){
 
 }
 
+// this handles the toggling between good and bad dog
 function handleToggle(event){
-  // debugger
-  // console.log(event)
   let goodDog = true
-  if (event.target.innerText.charAt(0) === "G"){
-    goodDog = false
-  } else {
-    goodDog = true
-  }
+
+  // set up the attribute to be updated
+  event.target.innerText.charAt(0) === "G" ? goodDog = false : goodDog = true
   const formData = {
     isGoodDog: goodDog
   }
@@ -102,24 +98,15 @@ function handleToggle(event){
   fetch(`http://localhost:3000/pups/${event.target.dataset.dogId}`, reqObj)
   .then(resp => resp.json())
   .then(dog => updateDog(event, dog))
-
-
 }
 
+// this updates the good/bad label
 function updateDog(event, dog){
   let dg = ""
-  // debugger
-  if (dog.isGoodDog) {
-    dg = "Good Dog!"
-    allPups[parseInt(event.target.dataset.dogId) - 1]["isGoodDog"] = true
-  } else {
-    dg = "Bad Dog!"
-    allPups[parseInt(event.target.dataset.dogId) - 1]["isGoodDog"] = false
-  }
-
+  dog.isGoodDog ? dg = "Good Dog!" : dg = "Bad Dog!"
   event.target.innerText = dg
-  if (filterOn === "on"){
-    renderPups(allPups)
-  }
+  
+  // refresh the dog bar with updated data
+  getPups()
 }
 main()

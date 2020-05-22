9593154071a4fa9f148a7ae6c68f3823dcc39961
jequezinho <jroverby92@gmail.com>
2020-05-21 23:29:59 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-const PUPS_URL=" http://localhost:3000/pups"
+const PUPS_URL = "http://localhost:3000/pups"
 
 document.addEventListener("DOMContentLoaded", e =>
     {
+        const dogArray = []
+
         const dogBar = document.getElementById('dog-bar')
         const dogInfoContainer = document.getElementById('dog-info')
         const goodDogFilter = document.getElementById('good-dog-filter')
@@ -14,13 +16,14 @@ document.addEventListener("DOMContentLoaded", e =>
             dogInfoContainer['dog-id'] = randDog.id
             dogInfoContainer['is-good-dog'] = randDog.isGoodDog
             dogInfoContainer.innerHTML = `<img src=${randDog.image}><h2>${randDog.name}</h2><button>${randDog.isGoodDog ? 'Good' : 'Bad'} Dog!</button>`
-            dogs.forEach(
-            dog => dogBar.innerHTML += `<span style="display: block" id=${dog.id} is-good-dog=${dog.isGoodDog}>${dog.name}</span>`)
+            dogs.forEach(dog => {
+                dogArray.push(dog)
+                dogBar.innerHTML += `<span style="display: block" id=${dog.id} is-good-dog=${dog.isGoodDog}>${dog.name}</span>`
+            })
         })
 
         dogBar.addEventListener("click", e => {
             if (e.target.tagName === 'SPAN') {
-                console.log(e.target)
                 fetch(`${PUPS_URL}/${e.target.id}`)
                 .then(resp => resp.json())
                 .then(dog => {
@@ -35,17 +38,15 @@ document.addEventListener("DOMContentLoaded", e =>
             goodDogFilter.isOn = !goodDogFilter.isOn
             goodDogFilter.innerHTML = `Filter good dogs: ${goodDogFilter.isOn ? 'ON' : 'OFF'}`
             
-            for (const dog of dogBar.querySelectorAll('span')) {
-                dog.style.display = ((dog['is-good-dog'] || !goodDogFilter.isOn) ? 'block' : 'none')
-            }
+            const dogContainer = dogArray.filter(dog => dog.isGoodDog || !goodDogFilter.isOn)
+            dogBar.innerHTML = dogContainer.map(dog => `<span id=${dog.id} is-good-dog=${dog.isGoodDog}>${dog.name}</span>`).join('')
+
         })
 
         dogInfoContainer.addEventListener("click", e => {
             if (e.target.tagName === 'BUTTON') {
                 const myDog = e.target.parentNode
                 const myButton = myDog.querySelector('button')
-                console.log(myDog['is-good-dog'])
-                console.log(myDog['dog-id'])
 
                 const configObj = {
                     method: 'PATCH',
@@ -60,9 +61,9 @@ document.addEventListener("DOMContentLoaded", e =>
                 .then(resp => resp.json())
                 .then(dog => {
                     myDog['is-good-dog'] = dog.isGoodDog
+                    dogArray[dog.id - 1].isGoodDog = dog.isGoodDog
                     myButton.innerHTML = `${dog.isGoodDog ? 'Good' : 'Bad'} Dog!`
                     const myDogSpan = document.querySelector(`span[id='${dog.id}']`)
-                    console.log(myDogSpan, myDogSpan['is-good-dog'])
                     myDogSpan['is-good-dog'] = dog.isGoodDog
                 })
             }
